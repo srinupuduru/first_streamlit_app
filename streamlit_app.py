64ad3bef7b658b6621c742b20a71596f2386dd0c
SRINIVASAN A <111307253+srinupuduru@users.noreply.github.com>
2022-08-18 20:19:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,28 +9,3 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
-
-import requests
-streamlit.header("Fruityvice Fruit Advice!")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalized(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-streamlit.header('Fruityvice Fruit advice!')
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
