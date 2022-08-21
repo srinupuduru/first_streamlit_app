cba652dbdda8fde7899d79777910ac24ae9fe649
SRINIVASAN A <111307253+srinupuduru@users.noreply.github.com>
2022-08-22 00:31:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ else:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
 
-except URLError as e;
+except URLError as e:
      streamlit.error()
 #import requests
 
